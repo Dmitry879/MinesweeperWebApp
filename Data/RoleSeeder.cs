a5e489f91509688d734b8d07bdd1094027c1c3b8
Dmitry879 <dvaskovich@my.gcu.edu>
2025-10-14 19:33:54 -0400

    Initial commit

---
+++ Data/RoleSeeder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MinesweeperWebApp.Constants;
+
+namespace MinesweeperWebApp.Data
+{
+    public class RoleSeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (! await roleManager.RoleExistsAsync(Roles.Admin))
+               await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+
+            if (!await roleManager.RoleExistsAsync(Roles.Player))
+                await roleManager.CreateAsync(new IdentityRole(Roles.Player));
+        }
+    }
+}
